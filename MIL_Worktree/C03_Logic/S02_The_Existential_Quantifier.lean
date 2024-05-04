f4da7cbe3e453d828547175d37776853cef3f153
Benjamin F Jones <benjaminfjones@gmail.com>
2024-05-04 11:29:54 -0700

    Add exists proofs in C03S02

--- MIL_Worktree/C03_Logic/S02_The_Existential_Quantifier.lean
+++ MIL_Worktree/C03_Logic/S02_The_Existential_Quantifier.lean
@@ -41,6 +41,9 @@ theorem fnUb_add {f g : ℝ → ℝ} {a b : ℝ} (hfa : FnUb f a) (hgb : FnUb g 
     FnUb (fun x ↦ f x + g x) (a + b) :=
   fun x ↦ add_le_add (hfa x) (hgb x)
 
+theorem fnLb_add {f g : ℝ → ℝ} {a b : ℝ} (hfa : FnLb f a) (hgb : FnLb g b) :
+    FnLb (fun x ↦ f x + g x) (a + b) :=
+  fun x ↦ add_le_add (hfa x) (hgb x)
 section
 
 variable {f g : ℝ → ℝ}
@@ -52,10 +55,18 @@ example (ubf : FnHasUb f) (ubg : FnHasUb g) : FnHasUb fun x ↦ f x + g x := by
   apply fnUb_add ubfa ubgb
 
 example (lbf : FnHasLb f) (lbg : FnHasLb g) : FnHasLb fun x ↦ f x + g x := by
-  sorry
+  rcases lbf with ⟨ a, lbfa ⟩
+  rcases lbg with ⟨ b, lbgb ⟩
+  use a + b
+  apply fnLb_add lbfa lbgb
 
 example {c : ℝ} (ubf : FnHasUb f) (h : c ≥ 0) : FnHasUb fun x ↦ c * f x := by
-  sorry
+  rcases ubf with ⟨ a, ubfa ⟩
+  use c*a
+  intro x
+  apply mul_le_mul_of_nonneg_left
+  apply ubfa
+  assumption
 
 example : FnHasUb f → FnHasUb g → FnHasUb fun x ↦ f x + g x := by
   rintro ⟨a, ubfa⟩ ⟨b, ubgb⟩
@@ -129,7 +140,10 @@ example (divab : a ∣ b) (divbc : b ∣ c) : a ∣ c := by
   use d * e; ring
 
 example (divab : a ∣ b) (divac : a ∣ c) : a ∣ b + c := by
-  sorry
+  rcases divab with ⟨ j, rfl ⟩
+  rcases divac with ⟨ k, rfl ⟩
+  use j + k
+  rw [mul_add]
 
 end
 
@@ -143,7 +157,11 @@ example {c : ℝ} : Surjective fun x ↦ x + c := by
   dsimp; ring
 
 example {c : ℝ} (h : c ≠ 0) : Surjective fun x ↦ c * x := by
-  sorry
+  intro x
+  use x / c
+  dsimp
+  apply mul_div_cancel₀
+  assumption
 
 example (x y : ℝ) (h : x - y ≠ 0) : (x ^ 2 - y ^ 2) / (x - y) = x + y := by
   field_simp [h]
@@ -163,6 +181,12 @@ variable {α : Type*} {β : Type*} {γ : Type*}
 variable {g : β → γ} {f : α → β}
 
 example (surjg : Surjective g) (surjf : Surjective f) : Surjective fun x ↦ g (f x) := by
-  sorry
+  intro y
+  dsimp
+  show ∃ a, g (f a) = y
+  rcases surjg y with ⟨ z, hz ⟩
+  rcases surjf z with ⟨ w, hw ⟩
+  use w
+  rw [hw, hz]
 
 end
